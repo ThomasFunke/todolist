1a9c789ba9d4dca6937036ae4c93f1f60412feb6
sorin.irimies <sorin.irimies@moovel.com>
2018-11-26 16:03:37 +0100

    rename tag

--- src/main/kotlin/ro/sorin/todolist/util/logger.kt
+++ src/main/kotlin/ro/sorin/todolist/util/logger.kt
@@ -3,4 +3,4 @@ package ro.sorin.todolist.util
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-fun logger(id: String = "bitchat-server"): Logger = LoggerFactory.getLogger(id)
+fun logger(id: String = "todolist-server"): Logger = LoggerFactory.getLogger(id)
